23e17c2b3a3432632eb71c72c08ef689f642104a
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-03-30 20:08:51 -0300

    add config provider to centralize environment configuration

---
+++ backend/services/config.go
@@ -0,0 +1,73 @@
+package services
+
+import (
+	"log"
+	"os"
+)
+
+type Configs struct {
+	DatabaseUri   string
+	RedisUri      string
+	RedisPassword string
+	AppEnv        string
+	AppFork       bool
+	BindAddr      string
+	UseTls        bool
+	TlsCertPath   string
+	TlsKeyPath    string
+}
+
+var (
+	instance        *Configs = nil
+	requiredEnvVars []string = []string{"REDIS_URI", "DATABASE_URI"}
+)
+
+func GenerateFromEnv() {
+	for _, v := range requiredEnvVars {
+		if _, ok := os.LookupEnv(v); !ok {
+			log.Panicf("environment: variable %s is not set", v)
+		}
+	}
+	instance = &Configs{}
+
+	instance.DatabaseUri = os.Getenv("DATABASE_URI")
+	instance.RedisUri = os.Getenv("REDIS_URI")
+	instance.RedisPassword = os.Getenv("REDIS_PASSWORD")
+
+	if os.Getenv("APP_ENV") == "" {
+		instance.AppEnv = "development"
+	} else {
+		instance.AppEnv = os.Getenv("APP_ENV")
+	}
+
+	if os.Getenv("APP_FORK") == "" {
+		instance.AppFork = false
+	} else {
+		instance.AppFork = os.Getenv("APP_FORK") == "true"
+	}
+
+	if os.Getenv("BIND_ADDR") == "" {
+		instance.BindAddr = ":3333"
+	} else {
+		instance.BindAddr = os.Getenv("BIND_ADDR")
+	}
+
+	if os.Getenv("USE_TLS") == "true" {
+		if os.Getenv("TLS_CERT_PATH") == "" || os.Getenv("TLS_KEY_PATH") == "" {
+			log.Panicln("environment: TLS_CERT_PATH and TLS_KEY_PATH must be set if USE_TLS is true")
+		}
+		instance.TlsCertPath = os.Getenv("TLS_CERT_PATH")
+		instance.TlsKeyPath = os.Getenv("TLS_KEY_PATH")
+		instance.UseTls = true
+	} else {
+		instance.UseTls = false
+	}
+}
+
+func ConfigProvider() *Configs {
+	if instance == nil {
+		instance = &Configs{}
+	}
+
+	return instance
+}
