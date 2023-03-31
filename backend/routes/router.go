167f49dd9344ddb6b9437d50b3b73c2df5a26c80
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-03-31 00:42:13 -0300

    add new routes to the router

--- backend/routes/router.go
+++ backend/routes/router.go
@@ -3,11 +3,14 @@ package routes
 import (
 	"time"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
 	"github.com/zanz1n/ws-messaging-app/middlewares"
 	"github.com/zanz1n/ws-messaging-app/services"
 )
+
+var validate = validator.New()
 
 func NewRouter(app *fiber.App) {
 	if services.ConfigProvider().AppEnv == "development" {
@@ -22,6 +25,10 @@ func NewRouter(app *fiber.App) {
 
 	db, conn := services.NewDbProvider()
 
+	jwtService := services.NewJwtService()
+
+	authService := services.NewAuthService(db, jwtService)
+
 	publisher := services.NewRedisProvider()
 	subscriber := services.NewRedisProvider()
 
@@ -30,6 +37,9 @@ func NewRouter(app *fiber.App) {
 	messagingService := services.NewMessagingService(publisher, subscriber, db)
 
 	app.Use("/api/gateway", middlewares.NewWebsocketMiddleware())
+	app.Use("/api/gateway", middlewares.NewAuthMiddleware(authService))
 
+	app.Post("/api/auth/signin", PostSignIn(authService))
+	app.Post("/api/auth/signup", PostSignUp(authService, db))
 	app.Get("/api/gateway", websocket.New(ChatGateway(messagingService)))
 }
