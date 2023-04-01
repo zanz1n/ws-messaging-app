3a1b1146cf8be185871ae782564359e220f43898
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-04-01 12:56:57 -0300

    move page render to root div instead of body

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import "./index.css";
 
-ReactDOM.createRoot(document.body).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <StrictMode>
         <RouterProvider router={router} />
     </StrictMode>,
