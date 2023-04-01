09c9f89da42958a507c8b1e3bb6416eee109d126
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-03-31 21:28:17 -0300

    add router and empty pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 import "./index.css";
 
 ReactDOM.createRoot(document.body).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
 );
