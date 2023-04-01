fcd702b7f9f50a1c5fbd63af0a427c8abff90f39
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-03-31 22:45:35 -0300

    update router to component approach

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,41 +1,21 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import App from "./App";
 import SignInPage from "./pages/SignIn.page";
 import SignUpPage from "./pages/SignUp.page";
+import ChatPage from "./pages/Chat.page";
 
-function ProtectedRoute({ children }: { children: any }) {
+function ProtectedRoute({ children }: { children: JSX.Element }) {
     return children;
 }
 
-export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App/>,
-        children: [
-            {
-                path: "/",
-                element: <ProtectedRoute>{""}</ProtectedRoute>
-            }
-        ]
-    },
-    {
-        path: "/auth/signin",
-        element: <App/>,
-        children: [
-            {
-                path: "/auth/signin",
-                element: <SignInPage/>
-            }
-        ]
-    },
-    {
-        path: "/auth/signup",
-        element: <App/>,
-        children: [
-            {
-                path: "/auth/signup",
-                element: <SignUpPage/>
-            }
-        ]
-    }
-]);
+function ErrorBoundary() {
+    return <div>{"Erro"}</div>;
+}
+
+export const router = createBrowserRouter(createRoutesFromElements(
+    <Route ErrorBoundary={ErrorBoundary} path="/" element={<App/>}>
+        <Route path="/" element={<ProtectedRoute><ChatPage/></ProtectedRoute>}/>
+        <Route path="auth/signin" element={<SignInPage/>}/>
+        <Route path="auth/signup" element={<SignUpPage/>}/>
+    </Route>
+));
