7cdc2cdc1f3063a63afc5e470dbf842304318b24
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-04-02 17:56:50 -0300

    add auth context available globally

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { AuthProvider } from "./lib/AuthContext";
 
 export default function App() {
     return (
-        <Outlet/>
+        <AuthProvider>
+            <Outlet/>
+        </AuthProvider>
     );
 }
