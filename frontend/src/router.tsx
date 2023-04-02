711c7af3dac2996caf5019ced39cc5302b389c37
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-04-02 19:58:27 -0300

    update router

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,19 +1,32 @@
-import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import { Route, createBrowserRouter, createRoutesFromElements, useNavigate } from "react-router-dom";
 import App from "./App";
 import SignInPage from "./pages/SignIn.page";
 import SignUpPage from "./pages/SignUp.page";
 import ChatPage from "./pages/Chat.page";
+import { useEffect } from "react";
+import { SocketProvider } from "./lib/SocketContext";
+import { ErrorBoundary } from "react-error-boundary";
 
-function ProtectedRoute({ children }: { children: JSX.Element }) {
-    return children;
+function RedirectFallback() {
+    const navigate = useNavigate();
+    useEffect(() => {
+        navigate("/auth/signin");
+    }, []);
+    return <></>;
 }
 
-function ErrorBoundary() {
-    return <div>{"Erro"}</div>;
+function ProtectedRoute({ children }: { children: JSX.Element }) {
+    return <>
+        <ErrorBoundary FallbackComponent={RedirectFallback}>
+            <SocketProvider>
+                {children}
+            </SocketProvider>
+        </ErrorBoundary>
+    </>;
 }
 
 export const router = createBrowserRouter(createRoutesFromElements(
-    <Route ErrorBoundary={ErrorBoundary} path="/" element={<App/>}>
+    <Route path="/" element={<App/>}>
         <Route path="/" element={<ProtectedRoute><ChatPage/></ProtectedRoute>}/>
         <Route path="auth/signin" element={<SignInPage/>}/>
         <Route path="auth/signup" element={<SignUpPage/>}/>
