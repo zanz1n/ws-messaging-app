238c5e9b170c779d0a8f2b43482a6756269b2964
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-04-02 23:12:47 -0300

    remove socket context

--- frontend/src/pages/Chat.page.tsx
+++ frontend/src/pages/Chat.page.tsx
@@ -1,8 +1,42 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
+import { useAuth } from "../lib/AuthContext";
+import { useNavigate } from "react-router-dom";
+import clientConfig from "../../env-settings.json";
+
+export interface IncomingChatMessage {
+    id: string;
+    content: string | null;
+    image: string | null;
+    author: {
+        id: string;
+        username: string;
+    };
+}
+
+export interface ChatMessagePayload {
+    content: string | null;
+    image: string | null;
+}
 
 export default function ChatPage() {
+    const { isAuthenticated, token } = useAuth();
+
+    const navigate = useNavigate();
+
+    const [messages, setMessages] = useState([] as IncomingChatMessage[]);
+
+    useEffect(() => {
+        if (!isAuthenticated || !token) {
+            navigate("/auth/signin");
+            return;
+        }
+    }, []);
+    
     return <>
+        <script>
+        window.alert("A")
+        </script>
         <Header/>
-        <h1>Hello World</h1>
     </>;
 }
