2a9a603a806a59ba3e1bdc744b51689ce9d24b41
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-04-10 20:36:54 -0300

    implemented the new heartbeat in the gateway

--- backend/routes/gateway.go
+++ backend/routes/gateway.go
@@ -1,48 +1,87 @@
 package routes
 
 import (
+	"os"
+
+	"github.com/goccy/go-json"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
 	"github.com/zanz1n/ws-messaging-app/services"
-	"github.com/zanz1n/ws-messaging-app/services/ws"
+	"github.com/zanz1n/ws-messaging-app/utils"
 )
 
-func ChatGateway(s *ws.WebsocketService) func(c *websocket.Conn) {
+func ChatGateway(s *services.WebsocketService) func(c *websocket.Conn) {
 	return func(c *websocket.Conn) {
-		defer c.Close()
-		var (
-			rawMsg []byte
-			err    error
-			user = c.Locals("user").(*services.UserJwtPayload)
-		)
-
 		connId := s.AddConn(c)
+
 		defer s.RemoveConn(connId)
+		defer c.Close()
 
-		for {
-			_, rawMsg, err = c.ReadMessage()
-
-			if err != nil {
-				err = c.WriteJSON(fiber.Map{
-					"error": err.Error(),
-				})
-				if err != nil {
-					break
-				}
-				continue
-			}
-
-			err = s.HandleRawPayload(&rawMsg, user)
-
-			if err != nil {
-				err = c.WriteJSON(fiber.Map{
-					"error": err.Error(),
-				})
-				if err != nil {
-					break
-				}
-				continue
-			}
+		if hostname, err := os.Hostname(); err == nil {
+			c.WriteJSON(fiber.Map{
+				"instanceId": hostname,
+				"heartbeat":  "10s",
+			})
 		}
+
+		heartbeat := utils.NewWebsocketHeartbeat(10)
+
+		go func() {
+			var (
+				err     error
+				payload []byte = make([]byte, 1024)
+				data    utils.HeartbeatPayload
+				size    int
+			)
+
+			for {
+				if _, payload, err = c.ReadMessage(); err != nil {
+					if err = c.WriteJSON(fiber.Map{
+						"error": err.Error(),
+					}); err != nil {
+						break
+					}
+					continue
+				}
+
+				if size = len(payload); size == 0 || size > 1024 {
+					if size > 1024 {
+						if err = c.WriteJSON(fiber.Map{
+							"error": "payload too large",
+						}); err != nil {
+							break
+						}
+					} else if size == 0 {
+						if err = c.WriteJSON(fiber.Map{
+							"error": "no data was sent",
+						}); err != nil {
+							break
+						}
+					}
+				}
+
+				data = utils.HeartbeatPayload{}
+
+				if err = json.Unmarshal(payload, &data); err != nil {
+					if err = c.WriteJSON(fiber.Map{
+						"error": err.Error(),
+					}); err != nil {
+						break
+					}
+					continue
+				}
+
+				if data.Type == "ping" {
+					heartbeat.Ping()
+					if err = c.WriteJSON(fiber.Map{
+						"type": "pong",
+					}); err != nil {
+						break
+					}
+				}
+			}
+		}()
+
+		heartbeat.Start(c)
 	}
 }
