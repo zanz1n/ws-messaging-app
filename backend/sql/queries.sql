4e2764d665f1f7a2c2cd34911f8b75774d508c40
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-04-11 20:21:47 -0300

    update queries.sql

--- backend/sql/queries.sql
+++ backend/sql/queries.sql
@@ -7,8 +7,8 @@ SELECT * FROM "user" WHERE "username" = $1;
 -- name: GetAllMessages :many
 SELECT * FROM "message" LIMIT $1;
 
--- name: GetMessagesByUser :many
-SELECT * FROM "message" WHERE "userId" = $1 LIMIT $2;
+-- name: GetMessageById :one
+SELECT * FROM "message" WHERE "id" = $1;
 
 -- name: DeleteMessageById :exec
 DELETE FROM "message" WHERE "id" = $1;
