faf0397d092e70e1b14b6e5196eab923df4d4638
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-04-12 21:32:41 -0300

    add post message method

--- backend/routes/messages.go
+++ backend/routes/messages.go
@@ -5,7 +5,12 @@ import (
 	"github.com/zanz1n/ws-messaging-app/services"
 )
 
-func DeleteMessage(ms *services.MessagesService) func (c *fiber.Ctx) error {
+type MessageIncomingData struct {
+	Content string `json:"content"`
+	Image   string `json:"image"`
+}
+
+func DeleteMessage(ms *services.MessagesService) func(c *fiber.Ctx) error {
 	return func(c *fiber.Ctx) error {
 		msgId := c.Params("id")
 
@@ -15,7 +20,7 @@ func DeleteMessage(ms *services.MessagesService) func (c *fiber.Ctx) error {
 			})
 		}
 
-		var user = c.Locals("user").(*services.UserJwtPayload)
+		user := c.Locals("user").(*services.UserJwtPayload)
 
 		if allowed, _ := ms.IsAllowed(user.ID, msgId); !allowed {
 			return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
@@ -31,7 +36,47 @@ func DeleteMessage(ms *services.MessagesService) func (c *fiber.Ctx) error {
 
 		return c.Status(fiber.StatusAccepted).JSON(fiber.Map{
 			"message": "message deleted",
-			"id": msgId,
+			"id":      msgId,
+		})
+	}
+}
+
+func PostMessage(ms *services.MessagesService) func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		var (
+			user = c.Locals("user").(*services.UserJwtPayload)
+			data MessageIncomingData
+			err  error
+		)
+
+		if err = c.BodyParser(&data); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		if err = validate.Struct(data); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		pubMsg := &services.CreateMessageDto{
+			User:    *user,
+			Content: data.Content,
+			Image:   data.Image,
+		}
+
+		msg, status, err := ms.Publish(pubMsg)
+
+		if err != nil {
+			return c.Status(status).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		return c.JSON(&fiber.Map{
+			"data": msg,
 		})
 	}
 }
