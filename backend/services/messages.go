e0ceb586b57880cdef8147f0c81d68488644d492
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-04-12 22:39:11 -0300

    messages service and its types splitted to different files

--- backend/services/messages.go
+++ backend/services/messages.go
@@ -12,39 +12,6 @@ import (
 	"github.com/zanz1n/ws-messaging-app/utils"
 )
 
-type CreateMessageDto struct {
-	Content string         `json:"content"`
-	Image   string         `json:"image"`
-	User    UserJwtPayload `json:"user"`
-}
-
-type MessageCreatePayload struct {
-	Type    string         `json:"type"`
-	ID      string         `json:"id"`
-	User    UserJwtPayload `json:"user"`
-	Image   *string        `json:"image"`
-	Content *string        `json:"content"`
-}
-
-type UserReturnedOnMessage struct {
-	ID       string `json:"id"`
-	Username string `json:"username"`
-}
-
-type MessageCreateReturnedData struct {
-	ID        string    `json:"id"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
-	User      UserReturnedOnMessage
-	Image     *string `json:"image"`
-	Content   *string `json:"content"`
-}
-
-type MessageDeletePayload struct {
-	Type string `json:"type"`
-	ID   string `json:"id"`
-}
-
 type MessagesService struct {
 	db *dba.Queries
 	ws *WebsocketService
