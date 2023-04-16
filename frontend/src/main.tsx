a386193e3bdd631e7d65d9f5cf794b0f6645fe52
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-04-16 16:27:10 -0300

    remove strict routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,8 @@
-import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <StrictMode>
-        <RouterProvider router={router} />
-    </StrictMode>,
+    <RouterProvider router={router} />
 );
