5b231a1158e613823cb8bedce40b408088ed1d45
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-04-16 21:29:27 -0300

    remove unecessary code

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,27 +1,9 @@
-import { Route, createBrowserRouter, createRoutesFromElements, useNavigate } from "react-router-dom";
+import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import App from "./App";
 import SignInPage from "./pages/SignIn.page";
 import SignUpPage from "./pages/SignUp.page";
 import ChatPage from "./pages/Chat.page";
-import { useEffect } from "react";
-import { ErrorBoundary } from "react-error-boundary";
 import { SocketProvider } from "./lib/SocketContext";
-
-function RedirectFallback() {
-    const navigate = useNavigate();
-    useEffect(() => {
-        navigate("/auth/signin");
-    }, []);
-    return <></>;
-}
-
-function ProtectedRoute({ children }: { children: JSX.Element }) {
-    return <>
-        <ErrorBoundary FallbackComponent={RedirectFallback}>
-            {children}
-        </ErrorBoundary>
-    </>;
-}
 
 export const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<App/>}>
