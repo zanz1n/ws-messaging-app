2e57f76aac7b98cb055f2fa63585acbec2e9f872
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-04-17 21:41:42 -0300

    move route file

--- backend/routes/auth.go
+++ backend/routes/auth.go
@@ -1,11 +1,21 @@
 package routes
 
 import (
+	"context"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/zanz1n/ws-messaging-app/dba"
 	"github.com/zanz1n/ws-messaging-app/services"
+	"github.com/zanz1n/ws-messaging-app/utils"
 )
 
 type SignInBody struct {
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+type SignUpBody struct {
 	Username string `json:"username" validate:"required"`
 	Password string `json:"password" validate:"required"`
 }
@@ -38,6 +48,59 @@ func PostSignIn(as *services.AuthService) func(c *fiber.Ctx) error {
 
 		return c.Status(fiber.StatusOK).JSON(fiber.Map{
 			"token": jwtToken,
+		})
+	}
+}
+
+func PostSignUp(as *services.AuthService, db *dba.Queries) func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		var (
+			body SignUpBody
+			ctx  = context.Background()
+		)
+
+		if err := c.BodyParser(&body); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "invalid from body",
+			})
+		}
+
+		if err := validate.Struct(body); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "invalid from body",
+			})
+		}
+
+		now := time.Now().UnixMilli()
+
+		err := db.CreateUser(ctx, dba.CreateUserParams{
+			Username:  body.Username,
+			Password:  as.GenerateHash(body.Password),
+			CreatedAt: now,
+			UpdatedAt: now,
+			Role:      "USER",
+			ID:        utils.RandomId(),
+		})
+
+		if err != nil {
+			return c.Status(fiber.StatusConflict).JSON(fiber.Map{
+				"error": "username already taken",
+			})
+		}
+
+		token, _ := as.AuthenticateUser(body.Username, body.Password)
+
+		user, err := as.ValidateJwtToken(token)
+
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "something went wrong",
+			})
+		}
+
+		return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+			"user":  user,
+			"token": token,
 		})
 	}
 }
--- backend/routes/user.go
+++ backend/routes/user.go
@@ -1,72 +1,9 @@
 package routes
 
 import (
-	"context"
-	"time"
-
 	"github.com/gofiber/fiber/v2"
-	"github.com/zanz1n/ws-messaging-app/dba"
 	"github.com/zanz1n/ws-messaging-app/services"
-	"github.com/zanz1n/ws-messaging-app/utils"
 )
-
-type UserCreatePayload struct {
-	Username string `json:"username" validate:"required"`
-	Password string `json:"password" validate:"required"`
-}
-
-func PostSignUp(as *services.AuthService, db *dba.Queries) func(c *fiber.Ctx) error {
-	return func(c *fiber.Ctx) error {
-		var (
-			body UserCreatePayload
-			ctx  = context.Background()
-		)
-
-		if err := c.BodyParser(&body); err != nil {
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-				"error": "invalid from body",
-			})
-		}
-
-		if err := validate.Struct(body); err != nil {
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-				"error": "invalid from body",
-			})
-		}
-
-		now := time.Now().UnixMilli()
-
-		err := db.CreateUser(ctx, dba.CreateUserParams{
-			Username:  body.Username,
-			Password:  as.GenerateHash(body.Password),
-			CreatedAt: now,
-			UpdatedAt: now,
-			Role:      "USER",
-			ID:        utils.RandomId(),
-		})
-
-		if err != nil {
-			return c.Status(fiber.StatusConflict).JSON(fiber.Map{
-				"error": "username already taken",
-			})
-		}
-
-		token, _ := as.AuthenticateUser(body.Username, body.Password)
-
-		user, err := as.ValidateJwtToken(token)
-
-		if err != nil {
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-				"error": "something went wrong",
-			})
-		}
-
-		return c.Status(fiber.StatusCreated).JSON(fiber.Map{
-			"user":  user,
-			"token": token,
-		})
-	}
-}
 
 /*
  * GetSelf returns the user's information
