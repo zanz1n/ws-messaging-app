4a09e105a06cb1fad0e044408f8ae7f284800284
Izan Rodrigues <izanrodrigues999@gmail.com>
2023-04-17 22:06:41 -0300

    add random id length variable

--- backend/utils/random_id.go
+++ backend/utils/random_id.go
@@ -2,8 +2,10 @@ package utils
 
 import nanoid "github.com/matoous/go-nanoid/v2"
 
+const RandomIdLen = 18
+
 func RandomId() string {
-	id, err := nanoid.New(18)
+	id, err := nanoid.New(RandomIdLen)
 	if err != nil {
 		panic(err)
 	}
